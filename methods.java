fd2bef3a1fe528a7dd1932efea6c5e79346cf47f
Gabe Kroeger <gandliv@gmail.com>
2018-11-29 11:33:26 -0600

    made the methods

--- methods.java
+++ methods.java
@@ -7,27 +7,81 @@
  */
 public class methods
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class methods
-     */
-    public methods()
+    public static String getResponse(String statement)
     {
-        // initialise instance variables
-        x = 0;
+        return "wump";
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    public static String findHighPriority(String statement)
     {
-        // put your code here
-        return x + y;
+        
+        return "wip";
+    }
+    
+    public static String findLowPriority(String statement)
+    {
+        return "wip";
+    }
+    
+    public static void remember(String statement)
+    {
+        
+    }
+    
+    public static String getNonCommitalResponse(String statement)
+    {
+        return "wip";
+    }
+    
+    public static String maybeAddName(String statement)
+    {
+        return "wip";
+    }
+    
+    private int findKeyword(String statement, String goal, int startPos)
+    {
+        String phrase = statement.trim().toLowerCase();
+        goal = goal.toLowerCase();
+
+        // The only change to incorporate the startPos is in
+        // the line below
+        int psn = phrase.indexOf(goal, startPos);
+
+        // Refinement--make sure the goal isn't part of a
+        // word
+        while (psn >= 0)
+        {
+            // Find the string of length 1 before and after
+            // the word
+            String before = " ", after = " ";
+            if (psn > 0)
+            {
+                before = phrase.substring(psn - 1, psn);
+            }
+            if (psn + goal.length() < phrase.length())
+            {
+                after = phrase.substring(
+                        psn + goal.length(),
+                        psn + goal.length() + 1);
+            }
+
+            // If before and after aren't letters, we've
+            // found the word
+            if (((before.compareTo("a") < 0) || (before
+                    .compareTo("z") > 0)) // before is not a
+                                            // letter
+                    && ((after.compareTo("a") < 0) || (after
+                            .compareTo("z") > 0)))
+            {
+                return psn;
+            }
+
+            // The last position didn't work, so let's find
+            // the next, if there is one.
+            psn = phrase.indexOf(goal, psn + 1);
+
+        }
+
+        return -1;
     }
 }
