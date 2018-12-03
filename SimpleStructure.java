5d0a00fd1970eeb738c1646aedce9c454e7492b3
Avery Cook <acook0011@myduneland.org>
2018-12-03 11:54:41 -0600

    transformIWantStatement

--- SimpleStructure.java
+++ SimpleStructure.java
@@ -8,6 +8,8 @@
 public class SimpleStructure
 {
     //Methods called from findSimpleSentenceStructure()
+    
+    
     private String transformIWantToStatement(String statement)
     {
         //  Remove the final period, if there is one
@@ -23,5 +25,25 @@ public class SimpleStructure
 	int psn = Methods.findKeyword(statement, "I want to", 0);
 	String restOfStatement = statement.substring(psn + 9).trim();
 	return "What would it mean to " + restOfStatement + "?";
+    }
+    
+    private String transformIWantStatement(String statement)
+    {
+	//  Remove the final period, if there is one
+	statement = statement.trim();
+	String lastChar = statement.substring(statement
+				.length() - 1);
+	if (lastChar.equals("."))
+	{
+		statement = statement.substring(0, statement.length() - 1);
+	}
+		
+
+	int psn = Methods.findKeyword (statement, "I want", 0);
+	String restOfStatement = statement.substring(psn + 7).trim();
+	if (restOfStatement.equals("you")){
+		restOfStatement = "me";
+	}
+	return "Would you really be happy if you had " + restOfStatement + "?";  
     }
 }
