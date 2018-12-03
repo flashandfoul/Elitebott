874fb4ec5d27d559451cab572b27ed30c26ba2d7
Joe Adcock <jradcock2002@gmail.com>
2018-12-03 12:19:20 -0600

    ssssimple

--- Methods.java
+++ Methods.java
@@ -73,6 +73,7 @@ public class Methods
         {
             simpleResponse = SimpleStructure.transformIWantStatement(statement);
         }
+        
         int psnYou = findKeyword(statement, "you", 0);
         int psnI = findKeyword(statement, "I", 0);
         if (psnYou >= 0
@@ -85,7 +86,10 @@ public class Methods
         {
             simpleResponse = SimpleStructure.transformIYouStatement(statement);
         }
-        
+        else if (findKeyword(statement, "I will", 0) >= 0)
+        {
+            simpleResponse = SimpleStructure.transformIWillStatement(statement);
+        }
         //return forumlated response
         return simpleResponse;
     }
