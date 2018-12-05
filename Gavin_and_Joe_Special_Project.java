01d1c8761332efc0c0591f74096657e5561e18bd
Joe Adcock <jradcock2002@gmail.com>
2018-12-05 11:55:50 -0600

    new project class special

---
+++ Gavin_and_Joe_Special_Project.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Gavin_and_Joe_Special_Project here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Gavin_and_Joe_Special_Project
+{
+   //hi
+}
