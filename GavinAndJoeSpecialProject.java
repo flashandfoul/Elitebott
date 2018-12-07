fa552b836872ad1a786dd8506b7979d1e2700ff0
Gavin Dooley <gdooley0096@myduneland.org>
2018-12-07 11:42:20 -0600

    asdf

--- GavinAndJoeSpecialProject.java
+++ GavinAndJoeSpecialProject.java
@@ -11,6 +11,7 @@ public class GavinAndJoeSpecialProject
     static String longWord;
     //Finds longest word.
     public static String findLongestWord(String statement){
+        //Finds longest word.
         int lastSpace = 0;
         int spacePsn = statement.indexOf(" ");
         longWord = statement.substring(0,spacePsn);
@@ -19,9 +20,54 @@ public class GavinAndJoeSpecialProject
             lastSpace = spacePsn;
             spacePsn = statement.indexOf(" ", spacePsn+1);
             if (spacePsn - lastSpace > longLength)
-                longWord = statement.substring(lastSpace, spacePsn);
-                longLength = longWord.length();
+                longWord = statement.substring(lastSpace+1, spacePsn);
+            longLength = longWord.length();
         }
+        if (statement.substring(lastSpace+1).length() > longWord.length())
+            longWord = statement.substring(lastSpace+1);
+        
+        //Removes punctuation.
+        if (longWord.indexOf(".") == 0)
+            longWord = longWord.substring(1);
+        if (longWord.indexOf(".") > 0)
+            longWord = longWord.substring(0,longWord.indexOf("."));
+        if (longWord.indexOf(",") == 0)
+            longWord = longWord.substring(1);
+        if (longWord.indexOf(",") > 0)
+            longWord = longWord.substring(0,longWord.indexOf(","));
+        if (longWord.indexOf("(") == 0)
+            longWord = longWord.substring(1);
+        if (longWord.indexOf("(") > 0)
+            longWord = longWord.substring(0,longWord.indexOf("("));
+        if (longWord.indexOf(")") == 0)
+            longWord = longWord.substring(1);
+        if (longWord.indexOf(")") > 0)
+            longWord = longWord.substring(0,longWord.indexOf(")"));
+        if (longWord.indexOf("?") == 0)
+            longWord = longWord.substring(1);
+        if (longWord.indexOf("?") > 0)
+            longWord = longWord.substring(0,longWord.indexOf("?"));
+        if (longWord.indexOf("!") == 0)
+            longWord = longWord.substring(1);
+        if (longWord.indexOf("!") > 0)
+            longWord = longWord.substring(0,longWord.indexOf("!"));
+        if (longWord.indexOf(";") == 0)
+            longWord = longWord.substring(1);
+        if (longWord.indexOf(";") > 0)
+            longWord = longWord.substring(0,longWord.indexOf(";"));
+        if (longWord.indexOf("{") == 0)
+            longWord = longWord.substring(1);
+        if (longWord.indexOf("{") > 0)
+            longWord = longWord.substring(0,longWord.indexOf("{"));
+        if (longWord.indexOf("}") == 0)
+            longWord = longWord.substring(1);
+        if (longWord.indexOf("}") > 0)
+            longWord = longWord.substring(0,longWord.indexOf("}"));
+        if (longWord.indexOf("\"") == 0)
+            longWord = longWord.substring(1);
+        if (longWord.indexOf("\"") > 0)
+            longWord = longWord.substring(0,longWord.indexOf("\""));
+        
         return longWord;
     }
     //Sends longest word to a text document for storage
